f3d947f231648415c90b2403016f45a2e9e2853b
ariefwl <ariefwl79@gmail.com>
2025-11-09 12:54:13 +0700

    perbaikan indek.ja

--- src/index.js
+++ src/index.js
@@ -68,6 +68,6 @@ app.get('/api/databases', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
